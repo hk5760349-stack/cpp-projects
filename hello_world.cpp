6d29405f9fd87dd026bdef3cf19e726e3caaa58f
naseerkhan_.1 <hk5760349@gmail.com>
2026-06-30 02:43:43 +0500

    Add hello_world.cpp with a greeting message

---
+++ hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, AI Course!" << endl;
+    cout << "This is the original code on main computer" << endl;
+    return 0;
+}
